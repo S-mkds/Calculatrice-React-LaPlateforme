f28934192675e08bd98d0e0ef74a2eb35ada5905
Samir Mokaddem <mokaddem-s@hotmail.com>
2022-09-15 19:49:05 +0200

    first commit

---
+++ front/Calculator9000/src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    
+    <div className="App">
+      <h1 className="NameProject">calculator 9000</h1>
+      <div className="calculator"></div>
+    </div>
+  );
+}
+
+export default App;
